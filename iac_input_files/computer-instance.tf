ad82d237f6ee184ec243ef2b23fae38216dfdd58
fpwilliams71 <fpwilliams71@yahoo.com>
2023-04-03 20:30:53 +0100

    Update computer-instance.tf

--- iac_input_files/computer-instance.tf
+++ iac_input_files/computer-instance.tf
@@ -14,9 +14,7 @@ resource "google_compute_instance" "default" {
   network_interface {
     network = "default"
 
-    access_config {
-      // Ephemeral public IP
-    }
+    
   }
 
   metadata = {
